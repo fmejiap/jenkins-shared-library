6b841595921ede39625c46f90534409f2169f4d8
Fred Mejia <fredy.mejia@outlook.com>
2024-04-20 21:30:07 +0200

    Update Get-ContainFolder.ps1

--- resources/com/planetpope/scripts/windows/Get-ContainFolder.ps1
+++ resources/com/planetpope/scripts/windows/Get-ContainFolder.ps1
@@ -1,10 +1,1 @@
-# Define la ruta del directorio que quieres explorar
-$directorio = "C:\SQL2019"
-
-# Usa Get-ChildItem para obtener el contenido del directorio
-$contenido = Get-ChildItem -Path $directorio
-
-# Muestra el contenido del directorio
-foreach ($item in $contenido) {
-    Write-Host $item.Name
-}
+Get-Timezone 
