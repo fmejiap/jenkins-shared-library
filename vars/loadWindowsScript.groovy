02e32d8c5c67374169de320745e9cc91e3c0003b
Fred Mejia <fredy.mejia@outlook.com>
2024-04-20 16:20:42 +0200

    Create loadWindowsScript.groovy

---
+++ vars/loadWindowsScript.groovy
@@ -0,0 +1,4 @@
+def call(Map config = [:]) { 
+  def scriptcontents = libraryResource "com/planetpope/scripts/windows/${config.name}"    
+  writeFile file: "${config.name}", text: scriptcontents
+} 
