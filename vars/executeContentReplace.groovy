2d0028044b1cf34dcb7e5abf0384ec7de8190322
Fred Mejia <fredy.mejia@outlook.com>
2024-04-29 23:34:00 +0200

    Update executeContentReplace.groovy

--- vars/executeContentReplace.groovy
+++ vars/executeContentReplace.groovy
@@ -14,7 +14,7 @@ def call(Map config = [:]) {
             fileContentReplaceConfig(
                 configs: fileReplaceConfigs,
                 fileEncoding: 'UTF-8',
-                filePath: config.filePath
+                filePath: config.filesPath
             )
         ])
 }
