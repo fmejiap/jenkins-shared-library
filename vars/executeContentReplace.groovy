f32f4619c2ae53787c324a34755cb8fcc155def4
Fred Mejia <fredy.mejia@outlook.com>
2024-04-29 23:22:30 +0200

    Create executeContentReplace.groovy

---
+++ vars/executeContentReplace.groovy
@@ -0,0 +1,20 @@
+def call(Map config = [:]) {
+    def fileReplaceConfigs = []
+    for (item in config.replaceConfigs) {
+        def fileConfig = fileContentReplaceItemConfig(
+                replace: item.replace,
+                search: item.search,
+                verbose: true
+            )
+
+        fileReplaceConfigs.add(fileConfig)
+    }
+
+    contentReplace(configs: [
+            fileContentReplaceConfig(
+                configs: fileReplaceConfigs,
+                fileEncoding: 'UTF-8',
+                filePath: config.filePath
+            )
+        ])
+}
