023e5f1af72226a66334df5753a7c8370f6eb016
Fred Mejia <fredy.mejia@outlook.com>
2024-04-29 21:58:49 +0200

    Update addOrUpdateIisEnvironmentVariables.groovy

--- vars/addOrUpdateIisEnvironmentVariables.groovy
+++ vars/addOrUpdateIisEnvironmentVariables.groovy
@@ -3,7 +3,7 @@ def call (Map config = [:]) {
   
   if (config.environmentVariables != null)
   {
-    for (item in environmentVariables)
+    for (item in config.environmentVariables)
     {
       executeiisAppCmd.call("clear config "+config.siteName + " -section:system.webServer/aspNetCore //environmentVariables:[] /commit:apphost")
       executeiisAppCmd.call("set config "+config.siteName + " -section:system.webServer/aspNetCore /+" + "environmentVariables.[name='${item.name}',value='${item.value}']"+" /commit:apphost")
