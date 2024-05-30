e9ad151fa01952879b13d32c6700835d321f7ff3
Fred Mejia <fredy.mejia@outlook.com>
2024-05-30 14:39:00 +0200

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -2,22 +2,22 @@ def info(Map config = [:]) {
     echo "INFO: ${config.message}"
 }
 
-def warning(message) {
+def warning(Map config = [:]) {
     echo "WARN: ${config.message}"
 }
 
-def debug(message) {
+def debug(Map config = [:]) {
     echo "DEBUG: ${config.message}"
 }
 
-def error(message) {
+def error(Map config = [:]) {
     echo "ERROR: ${config.message}"
 }
 
-def trace(message) {
+def trace(Map config = [:]) {
     echo "TRACE: ${config.message}"
 }
 
-def fatal(message) {
+def fatal(Map config = [:]) {
     echo "FATAL: ${config.message}"
 }
