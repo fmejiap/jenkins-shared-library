23d177dc53c8fc2136b2c5260e6343a5bc1fee56
Fred Mejia <fredy.mejia@outlook.com>
2024-06-06 11:46:43 +0200

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,3 @@
-import com.planetapope.exceptions.ValidationException
-
 def call(Map config = [:]) {
     throw new ValidationException("Error...!")
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
