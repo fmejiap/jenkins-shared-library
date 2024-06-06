dedec77afe7006f527859b6c786398ddfc5590ca
Fred Mejia <fredy.mejia@outlook.com>
2024-06-06 18:58:32 +0200

    Update Computer.groovy

--- src/com/planetapope/computeraggregate/Computer.groovy
+++ src/com/planetapope/computeraggregate/Computer.groovy
@@ -7,17 +7,5 @@ public class Computer {
 		this.model=model
 	}
 	//Builder Class
-	public static class ComputerBuilder{
-		private String model
-		public ComputerBuilder setModel(model) {
-			this.model = model;
-			return this;
-		}
-		
-		public Computer build(){
-			return new Computer(this.model);
-		}
-
-	}
 
 }
