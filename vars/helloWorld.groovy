4d8c5201795e227929fa8929d050ef0b3dc44c07
Fred Mejia <fredy.mejia@outlook.com>
2024-06-06 18:50:22 +0200

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,9 @@
 import com.planetapope.exceptions.ValidationException
-
+import com.planetapope.computeraggregate.Computer
 def call(Map config = [:]) {
-    throw new ValidationException ("Error...!")
+    def comp = new Computer.ComputerBuilder().setModel("TESTTTTS").build()
+    def values=comp.model
+				println "zzzz > ${values}"
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
     
 }
