b26e6d7d8a181037e5ce291ee2d366a887ac3440
Fred Mejia <fredy.mejia@outlook.com>
2024-06-09 21:44:40 +0200

    Update pipelineBehavior.groovy

--- vars/pipelineBehavior.groovy
+++ vars/pipelineBehavior.groovy
@@ -1,13 +1,13 @@
-def call (Map config =[:]){
+def approveAndChecks (Map config =[:]){
   def approval = null
   timeout(time: 15, unit: "MINUTES") {
-    approval = input(id: 'wait-approval',message: 'Approve?', submitterParameter: 'approver', parameters: [choice(choices: ['Cancel', 'Deploy'], description: 'Are you sure?', name: 'choice')])
+    approval = input(id: 'wait-approval',message: 'Waiting for approval', submitterParameter: 'approver', parameters: [choice(choices: ['Reject', 'Approve'], description: 'Are you sure?', name: 'choice')])
   }
   try {
-    if (approval['choice'] == 'Deploy') {
-      print('choice deploy')
+    if (approval['choice'] == 'Approve') {
+      print('choice Approve')
     } else {
-      throw new Exception('Choosed cancel')
+      throw new Exception('Choosed Reject')
     }
   }
   catch (Exception ex){
