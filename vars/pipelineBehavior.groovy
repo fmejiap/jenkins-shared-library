b2c63dfee508bee91ae22e4122018939852f1a99
Fred Mejia <fredy.mejia@outlook.com>
2024-06-09 21:33:48 +0200

    Update pipelineBehavior.groovy

--- vars/pipelineBehavior.groovy
+++ vars/pipelineBehavior.groovy
@@ -1,3 +1,16 @@
 def call (Map config =[:]){
-  
+  def approval = null
+  timeout(time: 15, unit: "MINUTES") {
+    approval = input(id: 'wait-approval',message: 'Approve?', submitterParameter: 'approver', parameters: [choice(choices: ['Cancel', 'Deploy'], description: 'Are you sure?', name: 'choice')])
+  }
+  try {
+    if (approval['choice'] == 'Deploy') {
+      print('choice deploy')
+    } else {
+      throw new Exception('Choosed cancel')
+    }
+  }
+  catch (Exception ex){
+    currentBuild.result = 'Fail'
+  }
 }
