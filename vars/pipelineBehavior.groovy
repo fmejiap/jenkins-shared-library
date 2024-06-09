db75d49a260cc20da781f4f1183dd12d52046ebc
Fred Mejia <fredy.mejia@outlook.com>
2024-06-10 00:04:19 +0200

    Update pipelineBehavior.groovy

--- vars/pipelineBehavior.groovy
+++ vars/pipelineBehavior.groovy
@@ -1,16 +1,49 @@
-def approveAndChecks (Map config =[:]){
-  def approval = null
-  timeout(time: 15, unit: "MINUTES") {
-    approval = input(id: 'wait-approval',message: 'Waiting for approval', submitterParameter: 'approver', parameters: [choice(choices: ['Reject', 'Approve'], description: 'Are you sure?', name: 'choice')])
-  }
-  try {
-    if (approval['choice'] == 'Approve') {
-      print('choice Approve')
-    } else {
-      throw new Exception('Choosed Reject')
+def validatePipelineApproval(Map config = [:]) {
+    log.info message: 'Checking if approval is needed...'
+
+    if (config.needsApproval) {
+        log.info message: 'Approval needed'
+        def approval = null
+        try {
+            timeout(time: 15, unit: 'MINUTES') {
+                approval = input(id: 'wait-approval',
+                                message: 'Waiting for approval',
+                                submitterParameter: 'approver',
+                                parameters: [choice(choices: ['Reject', 'Approve'], description: 'Are you sure?', name: 'choice')])
+            }
+
+            if (approval['choice'] == 'Approve') {
+                log.info message: 'Choosed Approve'
+
+                Boolean isUserAllowed = validateDeployApprovedUser(deployApprovedUsers: config.deployApprovedUsers)
+                if (!isUserAllowed) {
+                    throw new Exception('User not allowed to run the pipeline')
+                }
+                log.info message: 'User is allowed to run the pipeline'
+            } else {
+                log.info message: 'Choosed Reject'
+                throw new Exception('Choosed Reject')
+            }
+        }
+        catch (Exception e) {
+            log.error message: e.getMessage()
+            currentBuild.result = 'Fail'
+        }
     }
-  }
-  catch (Exception ex){
-    currentBuild.result = 'Fail'
-  }
+    else {
+        log.info message: 'No approval needed'
+    }
+    log.info message: 'Approval checked!'
+}
+
+Boolean validateDeployApprovedUser(Map config = [:]) {
+    log.info message: 'Checking if user is allowed to run the pipeline...'
+
+    String userId = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause).getUserId()
+    if (!config.deployApprovedUsers.contains(userId)) {
+        log.error message: 'User not allowed to run the pipeline'
+        throw new Exception('User not allowed to run the pipeline')
+    }
+
+    return true
 }
