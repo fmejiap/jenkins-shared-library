ea2ef9a55a2bb3a882ec36512157e1cbfdcd8d19
Fred Mejia <fredy.mejia@outlook.com>
2024-06-10 08:59:25 +0200

    Update pipelineBehavior.groovy

--- vars/pipelineBehavior.groovy
+++ vars/pipelineBehavior.groovy
@@ -1,36 +1,39 @@
 def validatePipelineApproval(Map config = [:]) {
-    log.info message: 'Checking if approval is needed...'
-    def approval = null
-        try {
-            if (config.needApproval) {
-                timeout(time: 15, unit: 'MINUTES') {
+    try {
+        log.info message: 'Checking if approval is needed...'
+        def approval = null
+
+        if (config.needApproval) {
+            timeout(time: 15, unit: 'MINUTES') {
                 approval = input(id: 'wait-approval',
                                 message: '  Waiting for approval  ',
                                 submitterParameter: 'approver',
                                 parameters: [choice(choices: ['Reject', 'Approve'], description: 'Are you sure?', name: 'choice'),
-                                            text(name: 'comment', defaultValue: '', description: 'Enter some information')])
-                }
+                                text(name: 'comment', defaultValue: '', description: 'Enter some information')])
+            }
 
-                if (approval['choice'] == 'Approve') {
-                    log.info message: 'Choosed Approve'
-                    String userId = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause).getUserId()
-                    if (!config.deployApprovedUsers.contains(userId)) {
-                        throw new Exception('User not allowed to run the pipeline')
-                    }
-                } else {
-                    log.info message: 'Choosed Reject'
-                    throw new Exception('Choosed Reject')
+            if (approval['choice'] == 'Approve') {
+                log.info message: 'Choosed Approve'
+                log.info message: 'Comment: ' + approval['comment']
+
+                String userId = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause).getUserId()
+                if (!config.deployApprovedUsers.contains(userId)) {
+                    log.error message: 'User not allowed to run the pipeline'
+                    throw new Exception('User not allowed to run the pipeline')
                 }
+            } else {
+                log.info message: 'Comment: ' + approval['comment']
+                log.info message: 'Choosed Reject'
+                throw new Exception('Choosed Reject')
             }
-            else
-            {
-                log.info message: 'No approval needed'
-            }
-            
         }
-        catch (Exception ex)
-        {
-            log.error message: e.getMessage()
-            currentBuild.result = 'Fail'
+        else {
+            log.info message: 'No approval needed'
         }
+    }
+    catch (Exception ex)
+    {
+        log.error message: e.getMessage()
+        currentBuild.result = 'Fail'
+    }
 }
