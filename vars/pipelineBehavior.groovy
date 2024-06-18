b4b3b383b1422946228b0bbb6ebec4d12b512087
Fred Mejia <fredy.mejia@outlook.com>
2024-06-19 00:15:17 +0200

    Update pipelineBehavior.groovy

--- vars/pipelineBehavior.groovy
+++ vars/pipelineBehavior.groovy
@@ -20,11 +20,9 @@ def validatePipelineApproval(Map config = [:]) {
             } else {
                 log.info message: 'Choosed Reject'
                 log.info message: 'Comment: ' + userInputApproval['comment']
-                throw new org.jenkinsci.plugins.workflow.steps.FlowInterruptedException(
-                    org.jenkinsci.plugins.workflow.steps.FlowInterruptedException.Result.ABORTED,
-                    new org.jenkinsci.plugins.workflow.support.steps.input.Rejection(
-                        'User chose to reject the deployment'
-                    )
+                throw new org.jenkinsci.plugins.workflow.steps.FlowInterruptedException(org.jenkinsci.plugins.workflow.steps.FlowInterruptedException.Result.ABORTED,
+                                                                                        new org.jenkinsci.plugins.workflow.support.steps.input.Rejection('User chose to reject the deployment')
+                )
             }
         }
         else {
