47f69c2230897b40d1199344ce1215057c45cb6c
Fred Mejia <fredy.mejia@outlook.com>
2024-06-25 23:13:01 +0200

    Create Steps.groovy

---
+++ pipelines/Steps.groovy
@@ -0,0 +1,11 @@
+class Steps {
+
+    def helloWorldTask(message) {
+        try {
+          echo "Hi ${message}"
+        }
+        catch (Exception ex) {
+            throw ex
+        }
+    }
+}
